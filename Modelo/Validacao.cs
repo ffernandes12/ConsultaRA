fcae619980e48e6bbd0ab313fbf410af34baa4a0
nando <nando@LAPTOP-IP4HE595>
2024-04-23 23:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo/Validacao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaRA.Modelo
+{
+    internal class Validacao
+    {
+        private string RA = "n3425f0";
+        private string mensagem;
+        private string OutroRA;
+
+        public Validacao(string outroRA)
+        {
+            OutroRA = outroRA;
+            Verificar();
+        }
+
+        private void Verificar()
+        {
+            if (RA != OutroRA)
+            {
+                mensagem = "RA de outro aluno";
+
+            }
+
+            else
+            {
+                mensagem = "RA: Fernando Fernandes";
+            }
+        }
+
+        public string Mensagem { get => mensagem; }
+
+    }
+}
